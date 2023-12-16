bd50f86cd2394fbbc78a0cecbb29f7f8b9359ac2
poudelangeela <153889873+poudelangeela@users.noreply.github.com>
2023-12-15 19:40:12 -0600

    Delete code directory

--- code/arguments.py
+++
@@ -1,7 +0,0 @@
-data_files = ['STA221_Data summary.xlsx']
-project_folder = '/Users/angeelapoudel/Desktop/THESIS 2/'
-data_folder = project_folder + 'datafile/'
-train_folder = project_folder + 'train/'
-test_folder = project_folder + 'test/'
-label_col = 'FXTAS Diagnosis'
-pt_col = 'Study ID Number'
--- code/scripts.sh
+++
@@ -1,5 +0,0 @@
-python split_fold_form.py
-python feature_engg.py
-python feature_engg.py
-python featselect.py
-python holdout.py 
